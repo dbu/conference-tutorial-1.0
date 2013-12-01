0d842af99661732bb340b1f45e2d41dcc1f18bce
David Buchmann <david.buchmann@liip.ch>
2013-12-01 14:16:16 +0100

    add cmf bundles and minimal configuration for the cmf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,25 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // additional doctrine related bundles
+            new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
+            new Liip\DoctrineCacheBundle\LiipDoctrineCacheBundle(),
+
+            // cmf dependencies
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+
+            // cmf bundles
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+            new Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle(),
+            new Symfony\Cmf\Bundle\MenuBundle\CmfMenuBundle(),
+            new Symfony\Cmf\Bundle\ContentBundle\CmfContentBundle(),
+            new Symfony\Cmf\Bundle\SimpleCmsBundle\CmfSimpleCmsBundle(),
+            new Symfony\Cmf\Bundle\MediaBundle\CmfMediaBundle(),
+
+            // other helper bundles
+            new Lunetics\LocaleBundle\LuneticsLocaleBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
